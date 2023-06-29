cb67d9fe6d872cfbc778dca409294a4436e77d6f
Vladyslav-Potapenko <koksik.kun48@gmail.com>
2023-06-29 19:44:54 +0300

    homework5

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from currency.models import Rate, Contact_us
 
-def hello_world(request):
-    return HttpResponse('Hello World!!!')
+
+
+def rate_list(request):
+    results = []
+    rates = Rate.objects.all()
+    for rate in rates:
+        results.append(
+            f'{rate.id}'
+        )
+
+    return HttpResponse(str(results))
+
+def Contact_us_list(request):
+    results = []
+    contact_us_list = Contact_us.objects.all()
+    for contactus in contact_us_list:
+        results.append(
+            f'ID: {contactus.id}, Email:{contactus.email_from}, Subject: {contactus.subject}, Message: {contactus.message}'
+        )
+
+    return HttpResponse(str(results))
